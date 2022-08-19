c37f29fcac16009789023f5f15ab24ed844b4aa5
Oskar Malinowski <kikson.yy@gmail.com>
2022-08-19 23:43:56 +0200

    created webb

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const box = document.querySelector('.box')
 const img = document.querySelector('.img')
-const myPix = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"]
+const myPix = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg"]
 const shakeImg = () => {
     setTimeout(randomImg, 1100)
 
